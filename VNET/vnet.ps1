a43bb02a02c73e695fbf23bb1888248f60606c5f
Surya Teja Tirumalaraju <surya.teja.tirumalaraju@kdi.kongsberg.com>
2021-03-04 22:01:03 +0530

    first commit

---
+++ VNET/vnet.ps1
@@ -0,0 +1,14 @@
+﻿param 
+(
+    [Parameter(Mandatory=$True)][string]$rgn,
+    [Parameter(Mandatory=$True)][string]$loc,
+    [Parameter(Mandatory=$True)][string]$vNetworkName
+    #[Parameter(Mandatory=$True)][string]$vNetworkAddress
+)
+
+$PassParm = @{
+    vNetName = $vNetworkName
+    #IPPrefix = $vNetworkAddress
+}
+
+New-AzResourceGroupDeployment -Name "testdep01" -ResourceGroupName $rgn -TemplateFile .\vnet1.json -TemplateParameterObject $PassParm
